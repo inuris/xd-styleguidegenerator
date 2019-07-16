11ea429175664aee43a5569d41faee9a0d9101a2
Khoa Lam <lakhoa1989@gmail.com>
2019-07-16 10:11:21 +0700

    research

--- gitinit.bat
+++
@@ -1,7 +0,0 @@
-set /p URL=Enter remote repository URL:
-git init
-git add .
-git commit -m "Initialize"
-git remote add origin %URL%
-git remote -v
-git push origin master --force
--- main.js
+++ main.js
@@ -1,10 +1,48 @@
 const { Text, Color } = require("scenegraph");
 
+    
 function styleToTextHandlerFunction(selection) {
-    const textData = selection.items[0];
-    selection.items[0].text = `${textData.fontFamily} ${textData.fontStyle} ${textData.fontSize}px`;
+    let node = selection.items[0];
+    // let assets = require("assets");
+    // let allCharacterStyles = assets.characterStyles.get();
+    // let count = 0; 
+    // for (let i=0;i<allCharacterStyles.length;i++){        
+    //     let characterStyle = allCharacterStyles[i];
+    //     if (characterStyle.name.indexOf('@') === 0){ 
+    //         let name = characterStyle.name;           
+    //         let style= characterStyle.style;
+    //         let text = new Text();
+    //         text.text = `${name} - ${style.fontFamily} ${style.fontStyle} ${style.fontSize}px`;
+    //         text.styleRanges = [
+    //             {
+    //                 length: text.text.length,
+    //                 fill: new Color("black"),
+    //                 fontFamily: style.fontFamily,
+    //                 fontSize: style.fontSize,
+    //                 fontStyle: style.fontStyle
+    //             }
+    //         ];
+    //         selection.insertionParent.addChild(text);
+    //         text.moveInParentCoordinates(0, 100 * count++);
+    //     }
+    // }
+    if (node.text.indexOf(" ")>0)
+        node.text = node.text.substring(0,node.text.indexOf(" "));
+    node.text+= ` - ${node.fontFamily} ${node.fontStyle} ${node.fontSize}px`;
 }
-
+function printStyle(selection, currentStyle){
+    let text = new Text();
+    text.text = `${currentStyle.fontFamily} ${currentStyle.fontStyle} ${currentStyle.fontSize}px`;
+    // text.styleRanges = [
+    //     {
+    //         length: text.text.length,
+    //         fill: new Color("gray"),
+    //         fontSize: 20
+    //     }
+    // ];
+    selection.insertionParent.addChild(text);
+    text.moveInParentCoordinates(0, 100 * i);
+}
 function createRainbowTextHandlerFunction(selection) {
 
     const node = new Text();
